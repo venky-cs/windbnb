245b92c00e2330cb05f004d84c50c239407074e8
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-01-12 20:25:28 +0530

    'update-input-focusState'

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -3,7 +3,10 @@ import { UserContext } from '../App'
 
 function PopUp({close}) {
     const Context=useContext(UserContext)
-    const [focus,setFocus]=useState(false);
+    const [focus,setFocus]=useState({
+        city:false,
+        guest:false
+    });
     const [guest,setGuest]=useState('12');
     console.log(focus)
     console.log(Context.updateLocation)
@@ -12,20 +15,27 @@ function PopUp({close}) {
             <button className="close" onClick={() => close()}>X</button>
         <div className="pop-up">
             <div className="search">
-                <input type="text" name="" id="" placeholder="Add Location" value= {Context.location} onFocus={() => setFocus(true)}/>
-                <input type="text" name="" id="" placeholder="Add Guest" value={guest} onFocus={() => setFocus(true)}/>
+                <input type="text" name="" id="" placeholder="Add Location" value= {Context.location} onFocus={() => setFocus({city:true})}/>
+                <input type="text" name="" id="" placeholder="Add Guest" value={guest} onFocus={() => setFocus({guest:true})}/>
                 <button onClick={() =>setFocus(false)}>Search</button>
             </div>
 
             <div className="list">
-                {focus && Context.list.map(list => {
+                {focus.city && Context.list.map(list => {
                     return(
                     <>
                     <li onClick={(e) => Context.updateLocation(e)} value={list}>{list}</li>
                     </>
                     )
                 })}
+                
             </div>
+            
+                <div className="guest">
+                        {
+                            focus.guest && <h5>Guest</h5>
+                        }
+                </div>
 
         </div>
         </div>
