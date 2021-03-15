dddf24c7d60ce286394ba458e539ac7f4aba9f6c
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-03-15 19:37:39 +0530

    'prevent-keyboard'

--- src/components/PopUp.js
+++ src/components/PopUp.js
@@ -25,8 +25,9 @@ function PopUp({ close }) {
             {/* <button className="close" onClick={() => close()}>X</button> */}
             <div className="pop-up">
                 <div className="search">
-                    <input type="text" name="" id="" placeholder="Add Location" value={value} onMouseOver={() => setFocus({ city: true })} />
-                    <input type="text" name="" id="" placeholder="Add Guest" value={guest && `${guest} guests`} onMouseOver={() => setFocus({ guest: true })} />
+                    <input type="text" name="" id="" placeholder="Add Location" value={value} onMouseOver={() => setFocus({ city: true })} inputmode='none'/>
+                    <input type="text" name="" id="" placeholder="Add Guest" value={guest && `${guest} guests`} onMouseOver={() => setFocus({ guest: true })} 
+                    inputmode='none'/>
                     <button className="popup-btn" onClick={() => 
                         {
                         Context.updateLocation(value)
