72e52b7cc1aad4b10e683283a0a2f44354f5ab57
venkatesh <75250713+venky-cs@users.noreply.github.com>
2021-04-12 10:25:17 +0530

    'final-commit'

--- src/App.js
+++ src/App.js
@@ -1,46 +1,51 @@
-import React,{ useState } from 'react'
+import React, { useState } from 'react';
 import './App.css';
-import Header from './components/Header'
-import Main from './components/Main'
-import Footer from './components/Footer'
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
-export let UserContext =''
+export let UserContext = '';
 
 function App() {
-  const [location, setLocation] = useState("");
+  const [location, setLocation] = useState('');
 
   const list = [
     'Helsinki,Finland',
     'Turku,Finland',
     'Oulu,Finland',
-    'Vaasa,Finland'
-  ]
-  const [count,setCount] = useState('');
-  UserContext = React.createContext({location,list,updateLocation,count,updateCount})
+    'Vaasa,Finland',
+  ];
+  const [count, setCount] = useState('');
+  UserContext = React.createContext({
+    location,
+    list,
+    updateLocation,
+    count,
+    updateCount,
+  });
 
   return (
     <div className="App">
-      <UserContext.Provider value={{location,list,updateLocation,count,updateCount}}>
-      <Header />
-      <Main />
-      <Footer />
+      <UserContext.Provider
+        value={{ location, list, updateLocation, count, updateCount }}>
+        <Header />
+        <Main />
+        <Footer />
       </UserContext.Provider>
     </div>
   );
 
-  function updateLocation(e){
+  function updateLocation(e) {
     let value = e;
-    value =value.split(',')
+    value = value.split(',');
     // console.log(value[0])
-    setLocation(value[0])
+    setLocation(value[0]);
   }
 
-  function updateCount(e){
-    console.log(e)
-    setCount(`${e} guests`)
+  function updateCount(e) {
+    // console.log(e)
+    setCount(`${e} guests`);
   }
-
 }
-
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,55 +1,53 @@
-import React, { useState,useContext,useEffect } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import stays from '../stays.json';
-import { UserContext } from '../App'
+import { UserContext } from '../App';
 
 import StarRateIcon from '@material-ui/icons/StarRate';
 
-
 function Main() {
-    const [state, setState] = useState(stays)
-    const Context = useContext(UserContext)
-    let location =Context.location
-    location=location.split(',')
-    location=location[0]
+  const [state, setState] = useState(stays);
+  const Context = useContext(UserContext);
+  let location = Context.location;
+  location = location.split(',');
+  location = location[0];
 
-    useEffect(() => {
-        let filtered = stays.filter(stay => stay.city === location);
-        // console.log(filtered)
-        setState(filtered.length === 0 ?stays : filtered)
-    }, [location])
+  useEffect(() => {
+    let filtered = stays.filter((stay) => stay.city === location);
+    // console.log(filtered)
+    setState(filtered.length === 0 ? stays : filtered);
+  }, [location]);
 
-    return (
-        <div className="main">
-            <div className="head">
-            <h3 className="main-title">Stays in Finland</h3>
-            <p className="count">12+ stays</p>
+  return (
+    <div className="main">
+      <div className="head">
+        <h3 className="main-title">Stays in Finland</h3>
+        <p className="count">12+ stays</p>
+      </div>
+
+      <div className="cards">
+        {state.map((stay,i) => {
+          // console.log(stay)
+          return (
+            <div className="card" key={i}>
+              <img src={stay.photo} alt="pic" />
+              <div className="card-info">
+                {stay.superHost && <p className="super">super host</p>}
+                <p className="type">
+                  {stay.type}
+                  {stay.beds && ` .${stay.beds}beds`}
+                </p>
+                <p className="rating">
+                  <StarRateIcon id="red" />
+                  {stay.rating}
+                </p>
+              </div>
+              <h4 className="title">{stay.title}</h4>
             </div>
-
-
-            <div className="cards">
-            {state.map((stay) => {
-                // console.log(stay)
-                return (
-                <div className = "card" >
-                <img src={stay.photo} alt="pic"/>
-                <div className = "card-info">
-                    {stay.superHost &&
-                    <p className="super">super host</p>}
-                            <p className="type">{stay.type}{stay.beds && ` .${stay.beds}beds`}</p>
-                    <p className = "rating"><StarRateIcon id="red" />{stay.rating}</p>
-                </div>
-                <h4 className='title'>{stay.title}</h4>
-            </div>
-
-)
-            })}
-
-            </div>
-
-            
-        </div>
-    )
-
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default Main
+export default Main;
